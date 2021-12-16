0b4c5a50838f6541a35b0ceb39509c7ff3610a28
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-16 19:11:30 +0100

    Create variable

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## If we assign a name to a set of data, is called Variable
+
+# Syntax: VARNAME=DATA
+a=100       #Number
+b=apple     #String
+c=true      #Boolean
+
+Shell does not have any data type by default

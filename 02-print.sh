fcb584d6ed44292d51f3035f7581bc23102c6a35
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-16 18:41:03 +0100

    Echo command color codes

--- 02-print.sh
+++ 02-print.sh
@@ -11,4 +11,29 @@ echo Hello World
 echo Installing Nginx
 echo starting Nginx Service
 
+## We can print text in colors
+## Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+
+## -e - TO enale enable \e
+## \e - To enable colours
+## COL - Color code
+## m - End of syntax
+## 0 - TO disable color
+
+## Color Codes
+#Red       31
+#Green     32
+#Yellow    33
+#Blue      34
+#Magneta   35
+#Cyan      36
+
+
+
+echo -e "\e[31mHello In Red Color\e[0m"
+echo -e "\e[32mHello In Green Color\e[0m"
+echo -e "\e[33mHello In Yellow Color\e[0m"
+echo -e "\e[34mHello In Blue Color\e[0m"
+echo -e "\e[35mHello In Magneta Color\e[0m"
+echo -e "\e[36mHello In Cyan Color\e[0m"
 

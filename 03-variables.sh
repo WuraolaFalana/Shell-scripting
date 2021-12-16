3a0298e41eceb3041663d3a4a1fa876ff5e6a935
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-16 20:02:35 +0100

    Create vars and access them

--- 03-variables.sh
+++ 03-variables.sh
@@ -23,3 +23,16 @@ echo a in currency = ${a}USD
 #Use cases
 #1. If we want to use a value in multiple places then we go with variables. This brings an advantage of
 # changing the value in one place and it impacts in all the places.
+
+DATE=2021-12-08
+echo Good Morning, Welcome, Today date is ${DATE}
+
+## 2. If we need to get the data dynamically, we use variables to store that data and we refer
+   #i. Command substitution - VAR=$(command)
+   #ii. Arithmetic substitution - VAR=$((expression)).
+
+DATE=$(date +%F)
+echo Good Morning, Welcome, Today date is ${DATE}
+
+ADD=$((2+3))
+echo ADD = ${ADD}

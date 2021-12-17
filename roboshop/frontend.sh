16c169d82ea9348439ad87929e3d0a24d5d2f2ae
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-17 07:24:50 +0100

    Add root user check in frontend script

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -22,7 +22,7 @@
 #Check whether the script is running as root user or not
 UserID=$(id -u)
 if [ ${UserID} -ne 0 ]; then
-  echo -e "\e[1;31mYou should be a root user to perform this script\[0m"
+  echo -e "\e[1;31mYou should be a root user to perform this script\e[0m"
   exit
   fi
 yum install nginx -y

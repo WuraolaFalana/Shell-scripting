dd8f7e232c94cd9f0772718ce3711130a186d293
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-18 20:57:43 +0100

    Functions in roboshop

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -39,8 +39,10 @@ set -e
 echo Frontend Setup
 STATUS_CHECK() {
   if [ $1 -ne 0 ]; then               #$? means status check and $1 means first argument
-    echo -e "\e[1;31m${2}\e[0m"
+    echo -e "\e[1;31m${2} - FAILED\e[0m"
     exit 1
+  else
+    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
   fi
 }
 

a3067b10c56b0efd5eb05e07b1a4e8e63fe3dd2b
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-18 20:02:47 +0100

    Function scripts

--- 07-functions.sh
+++ 07-functions.sh
@@ -27,3 +27,11 @@ sample1
 # 2. Shell Built-in commands
 # 3. Alias
 #4. Functions
+
+## Functions have its own special variables
+sample2() {
+  echo First Argument = $1
+  echo Number of Arguments = $#
+}
+
+sample2 123 xyz

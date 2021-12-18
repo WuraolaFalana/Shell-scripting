8d99dc9121ca4db818f125e76500724531e20283
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-18 19:56:26 +0100

    Function scripts

---
+++ 07-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Variable - We assign a name to set of data is called a variable
+#Function - We assign a name to set of commands is called a function
+
+# Declare a function.
+#Method 1
+function sample() {
+  echo one
+  echo two
+
+}
+
+#Method2
+sample1() {
+  echo one
+  echo two
+}
+
+## Call the function
+sample
+sample1
+
+#Function just acts like command. It is one of four types of commands.
+
+# 1. Binaries
+# 2. Shell Built-in commands
+# 3. Alias
+#4. Functions

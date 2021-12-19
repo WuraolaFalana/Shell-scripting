c0939c5665723b5a2f623bd99e9c9b765da4bb33
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-19 15:12:53 +0100

    Frontend all code

---
+++ roboshop/components/common.sh
@@ -0,0 +1,11 @@
+LOG_FILE=/tmp/roboshop.log
+rm -f ${LOG_FILE}
+
+STATUS_CHECK() {
+  if [ $1 -ne 0 ]; then               #$? means status check and $1 means first argument
+    echo -e "\e[1;31m${2} - FAILED\e[0m"
+    exit 1
+  else
+    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
+  fi
+}
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -36,17 +36,10 @@ set -e
 
 echo Frontend Setup
 ## Check whether the script is running as root user or not
-LOG_FILE=/tmp/roboshop.log
-rm -f ${LOG_FILE}
 
-STATUS_CHECK() {
-  if [ $1 -ne 0 ]; then               #$? means status check and $1 means first argument
-    echo -e "\e[1;31m${2} - FAILED\e[0m"
-    exit 1
-  else
-    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
-  fi
-}
+# Source is nothing but import, like export command
+
+source components/common.sh
 
 yum install nginx -y &>>${LOG_FILE}
 STATUS_CHECK $? "Nginx Installation"

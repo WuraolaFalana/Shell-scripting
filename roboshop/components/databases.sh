82ec03e9cd48fc67d7e3188e6b865f923def9314
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-19 17:15:52 +0100

    Update Redis

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -31,6 +31,9 @@
   # mongo < catalogue.js
   # mongo < users.js
 
+
+
+
 ### MongoDB Set
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
 STATUS_CHECK $? "Download MongDB repo"
@@ -53,3 +56,29 @@ cd /tmp/mongodb-main
 mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
 STATUS_CHECK $? "Load Schema"
 
+
+
+
+#Install Redis.
+# curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+# yum install redis -y
+#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+
+#Start Redis Database
+
+# systemctl enable redis
+# systemctl start redis
+
+
+### Redis SetUp
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+STATUS_CHECK $? "Download Redis repo"
+
+yum install redis -y &>>${LOG_FILE}
+STATUS_CHECK $? "Install Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${LOG_FILE}   #Here we added -e option but we did not for MongoDB above
+STATUS_CHECK $? "Update Redis Config"
+
+systemctl enable redis &>>${LOG_FILE} &&  systemctl start redis &>>${LOG_FILE}
+STATUS_CHECK $? "Start Redis Service"

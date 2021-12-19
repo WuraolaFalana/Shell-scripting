07e3011f6d74dad7a15ff0673daaa48da32baa7c
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-19 19:02:03 +0100

    Update RabbitMQ script

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -8,6 +8,8 @@
   #fi
   source components/common.sh
 
+
+#-----------------------------------------------MONGODB---------------------------------------------
   #Setup MongoDB repos.
   # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
   #Install Mongo & Start Service.
@@ -33,8 +35,7 @@
 
 
 
-
-### MongoDB Set
+### MongoDB Setup
 echo -e "       ------->>>>>>>> \e[1;35mMongDB Setup\e[0m  <<<<<<<<---------"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
 STATUS_CHECK $? "Download MongDB repo"
@@ -58,7 +59,7 @@ mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
 STATUS_CHECK $? "Load Schema"
 
 
-
+#-----------------------------------------------REDIS---------------------------------------------
 
 #Install Redis.
 # curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
@@ -87,7 +88,7 @@ STATUS_CHECK $? "Start Redis Service"
 
 
 
-
+#-----------------------------------------------RABBITMQ---------------------------------------------
 #Erlang is a dependency which is needed for RabbitMQ.
 # yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
 #Setup YUM repositories for RabbitMQ.
@@ -136,6 +137,51 @@ STATUS_CHECK $? "Configure App User Permissions"
 #echo $ - This will give 0 if it found such user
 
 #Revert to lecture for loop from 1:10
+
+#---------------------------------------------MYSQL----------------------------------------
+#MySQL
+#MySQL is the database service which is needed for the application. So we need to install it and configure it for the application to work.
+
+#Manual Steps to Install MySQL
+#As per the Application need, we are choosing MySQL 5.7 version.
+
+#Setup MySQL Repo
+# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+
+#Install MySQL
+# yum install mysql-community-server -y
+
+#Start MySQL.
+# systemctl enable mysqld
+# systemctl start mysqld
+
+#Now a default root password will be generated and given in the log file.
+# grep temp /var/log/mysqld.log
+
+#Next, We need to change the default root password in order to start using the database service.
+# mysql_secure_installation
+
+#You can check the new password working or not using the following command.
+
+# mysql -u root -p
+
+#Run the following SQL commands to remove the password policy.
+#> uninstall plugin validate_password;
+#Setup Needed for Application.
+#As per the architecture diagram, MySQL is needed by
+
+#Shipping Service
+#So we need to load that schema into the database, So those applications will detect them and run accordingly.
+
+#To download schema, Use the following command
+
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+#Load the schema for Services.
+
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -u root -pRoboShop@1 <shipping.sql
 
 
 

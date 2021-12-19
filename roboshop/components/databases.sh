133b239c32dfbe007424e588b0237277df5dfd63
WuraolaFalana <95858284+WuraolaFalana@users.noreply.github.com>
2021-12-19 17:53:57 +0100

    Update RabbitMQ script

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -84,3 +84,43 @@ STATUS_CHECK $? "Update Redis Config"
 
 systemctl enable redis &>>${LOG_FILE} &&  systemctl start redis &>>${LOG_FILE}
 STATUS_CHECK $? "Start Redis Service"
+
+
+
+
+#Erlang is a dependency which is needed for RabbitMQ.
+# yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+#Setup YUM repositories for RabbitMQ.
+# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+#Install RabbitMQ
+# yum install rabbitmq-server -y
+#Start RabbitMQ
+# systemctl enable rabbitmq-server
+# systemctl start rabbitmq-server
+#RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence we need to create one user for the application.
+
+#Create application user
+# rabbitmqctl add_user roboshop roboshop123
+# rabbitmqctl set_user_tags roboshop administrator
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+#Ref link : https://www.rabbitmq.com/rabbitmqctl.8.html#User_Management
+
+
+### RabbitMQ SetUp
+echo -e "       ------->>>>>>>> \e[1;35mRabbitMQ Setup\e[0m  <<<<<<<<---------"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
+STATUS_CHECK $? "Download RabbitMQ Repo"
+
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG_FILE}  #Here we downloaded both the dependency and rabbitmq server at the same time.
+STATUS_CHECK $? "Install Erlang & RabbitMQ"
+
+systemctl enable rabbitmq-server &>>${LOG_FILE} &&  systemctl start rabbitmq-server &>>${LOG_FILE}
+STATUS_CHECK $? "Start RabbitMQ Service"
+
+rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+STATUS_CHECK $? "Create App User in RabbitMQ"
+
+# rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE}
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG_FILE}
+
